db4c3357c6396902bc073a4d1a22eba8fee68301
aldrin312 <aldrin_fernandez312@yahoo.com>
2018-12-09 17:17:30 -0500

    fix level 2 sceneload, fix player teleport

--- MapLevels/Assets/Scripts/StartZone.cs
+++ MapLevels/Assets/Scripts/StartZone.cs
@@ -18,10 +18,9 @@ public class StartZone : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
-        {
+
 
             other.transform.position = startposition.transform.position;
-        }
+        
     }
 }
